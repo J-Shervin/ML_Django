67a047d305fff4457857c6f1784f701155f132f9
Shervin <sh.j.shervin@gmail.com>
2022-11-14 13:10:25 +0330

    add ml code

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'endpoints', # apps
     'rest_framework', # add django rest framework
+    # apps
+    'endpoints',
+    'ml',
 ]
 
 MIDDLEWARE = [
